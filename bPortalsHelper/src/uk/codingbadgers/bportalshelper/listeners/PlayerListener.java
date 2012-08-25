5c13e8364de56f33b7723ae54696f20cfa477a5f
n3wton <n3wton@d3b929a1-8af9-456d-8fbe-6661ccc7830c>
2012-08-25 19:24:16 +0000

    [bPortalHelper] Added permissions, only handle right click interact events.
    git-svn-id: https://subversion.assembla.com/svn/bfundamentals@40 d3b929a1-8af9-456d-8fbe-6661ccc7830c

--- bPortalsHelper/src/uk/codingbadgers/bportalshelper/listeners/PlayerListener.java
+++ bPortalsHelper/src/uk/codingbadgers/bportalshelper/listeners/PlayerListener.java
@@ -5,10 +5,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import com.sk89q.worldedit.bukkit.selections.Selection;
 
 import uk.codingbadgers.bportalshelper.PortalPlayer;
@@ -24,23 +24,32 @@ public class PlayerListener implements Listener {
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		
+		// get the player who is interacting
 		final Player player = event.getPlayer();
 		if (player == null)
 			return;
 		
-		ItemStack itemInHand = player.getItemInHand();
-		if (itemInHand.getType() != Material.BOW)
+		// see if they have permission
+		if (!bPortalsHelper.hasPermission(player, "bportalshelper.use"))
 			return;
 		
+		// only handle right clicks
+		if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK)
+			return;
+		
+		// see if they are using a bow
+		ItemStack itemInHand = player.getItemInHand();
+		if (itemInHand.getType() != Material.SPIDER_EYE)
+			return;
+		
+		// get or create the portal player
 		PortalPlayer pPlayer = bPortalsHelper.getOrCreatePlayer(player);
 		if (pPlayer == null)
 			return;
 		
-		WorldEditPlugin we = bPortalsHelper.getWorldEdit();
-		if (we == null)
-			return;
+		// get the world edit selection
+		Selection portalSelection = bPortalsHelper.getWorldEdit().getSelection(player);
 		
-		Selection portalSelection = we.getSelection(player);		
 		pPlayer.addPortal(portalSelection);
 		
 	}
