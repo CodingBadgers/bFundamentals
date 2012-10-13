b37e143ebe5f31ada5c54011dcd9e78f61bcfa9d
n3wton <n3wton@d3b929a1-8af9-456d-8fbe-6661ccc7830c>
2012-10-13 23:55:39 +0000

    Added bLogBackup Module
    This does nothing yet. It's just the base.
    Plan is for a simple module that backs up the current log file on quit, and makes a new one.
    
    git-svn-id: https://subversion.assembla.com/svn/bfundamentals@84 d3b929a1-8af9-456d-8fbe-6661ccc7830c

---
+++ bLogBackup/src/uk/codingbadgers/blogbackup/bLogBackup.java
@@ -0,0 +1,21 @@
+package uk.codingbadgers.blogbackup;
+
+import uk.codingbadgers.bFundamentals.module.Module;
+
+public class bLogBackup extends Module {
+
+	/**
+	 * Called when the module is disabled.
+	 */
+	public void onDisable() {
+
+	}
+
+	/**
+	 * Called when the module is loaded.
+	 */
+	public void onEnable() {
+
+	}
+
+}
