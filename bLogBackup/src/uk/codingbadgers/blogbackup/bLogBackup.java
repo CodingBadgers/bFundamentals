47375a61b715ccb69ba1eb1c63a1ba015201fda1
n3wton <n3wton@d3b929a1-8af9-456d-8fbe-6661ccc7830c>
2012-10-15 21:54:13 +0000

    Wrote blogbackup
    bLogBackup will backup the log file on server stop to a backup folder, and clear the active log file.
    There is also a command to do the same, /logbackup
    
    git-svn-id: https://subversion.assembla.com/svn/bfundamentals@88 d3b929a1-8af9-456d-8fbe-6661ccc7830c

--- bLogBackup/src/uk/codingbadgers/blogbackup/bLogBackup.java
+++ bLogBackup/src/uk/codingbadgers/blogbackup/bLogBackup.java
@@ -1,5 +1,18 @@
 package uk.codingbadgers.blogbackup;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.bukkit.command.CommandSender;
+
+import uk.codingbadgers.bFundamentals.bFundamentals;
+import uk.codingbadgers.bFundamentals.commands.ModuleCommand;
 import uk.codingbadgers.bFundamentals.module.Module;
 
 public class bLogBackup extends Module {
@@ -8,7 +21,7 @@ public class bLogBackup extends Module {
 	 * Called when the module is disabled.
 	 */
 	public void onDisable() {
-
+		backupLog();
 	}
 
 	/**
@@ -16,6 +29,96 @@ public class bLogBackup extends Module {
 	 */
 	public void onEnable() {
 
+		registerCommand(new ModuleCommand("logbackup", "/logbackup").setHelp("Backup the log file."));
+		
 	}
+	
+	/**
+	 * Handle the command /logbackup
+	 */
+	public boolean onCommand(CommandSender sender, String label, String[] args) {
+		
+		if (!label.equalsIgnoreCase("logbackup"))
+			return false;
+		
+		if (!bFundamentals.getPermissions().has(sender, "bLogBackup.backup"))
+			return true;
+		
+		sender.sendMessage("[bLogBackup] Backing up log file...");
+		
+		final String backedupLogName = backupLog();
+		if (backedupLogName != null) {
+			sender.sendMessage("[bLogBackup] Backup complete");
+			sender.sendMessage("[bLogBackup] Backup can be found at...");
+			sender.sendMessage("[bLogBackup] " + backedupLogName);
+		} else {
+			sender.sendMessage("[bLogBackup] Backup failed");
+		}
+		
+		return true;
+		
+	}
+	
+	/**
+	 * Backs up the log file to a date+time log file. and clears the active log file
+	 */
+	private String backupLog() {
+		
+		final String currentDir = System.getProperty("user.dir");
+		
+		final File logFile = new File(currentDir + File.separatorChar + "server.log");
+			
+		final File logBackupFolder = new File(currentDir + File.separatorChar + "logBackups");
+		if (!logBackupFolder.exists())
+			logBackupFolder.mkdir();
+		
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy.HH-mm-ss");
+		Date date = new Date();		
+		final String backupLogName = currentDir + File.separatorChar + "logBackups" + File.separatorChar + "server." + dateFormat.format(date) + ".log";
+		
+		try {
+			copyFile(logFile, new File(backupLogName));
+		}
+		catch(Exception ex) {
+			return null;
+		}
+		
+		try {
+			FileOutputStream erasor = new FileOutputStream(currentDir + File.separatorChar + "server.log");
+			erasor.write((new String()).getBytes());
+			erasor.close();	
+		} catch (IOException e) {
+
+		}
+		
+		return backupLogName;
+	}
+	
+	/**
+	 * Copies a file from a source to a destination
+	 */
+	private void copyFile(File sourceFile, File destFile) throws IOException {
+	    if(!destFile.exists()) {
+	        destFile.createNewFile();
+	    }
+
+	    FileChannel source = null;
+	    FileChannel destination = null;
+
+	    try {
+	        source = new FileInputStream(sourceFile).getChannel();
+	        destination = new FileOutputStream(destFile).getChannel();
+	        destination.transferFrom(source, 0, source.size());
+	    }
+	    finally {
+	        if(source != null) {
+	            source.close();
+	        }
+	        if(destination != null) {
+	            destination.close();
+	        }
+	    }
+	}
+
 
 }
