d4763a78bf95542c4b602dbc5face0c838b8d468
James Fitzpatrick <thefishlive@hotmail.com>
2013-09-03 13:41:43 +0100

    Added license header plugin, will auto update the license headers every build

--- bPortals/src/uk/codingbadgers/bportals/PortalManager.java
+++ bPortals/src/uk/codingbadgers/bportals/PortalManager.java
@@ -1,3 +1,20 @@
+/**
+ * bPortals 1.2-SNAPSHOT
+ * Copyright (C) 2013  CodingBadgers <plugins@mcbadgercraft.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.codingbadgers.bportals;
 
 import java.util.HashSet;
@@ -6,6 +23,7 @@ import java.util.Set;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.util.NumberConversions;
 
 import com.google.common.collect.ImmutableSet;
 import com.sk89q.worldedit.Vector;
@@ -18,7 +36,7 @@ public class PortalManager {
 	public static PortalManager getInstance() {
 		return instance;
 	}
-	
+
 	public static void setInstance(PortalManager instance) {
 		PortalManager.instance = instance;
 	}
@@ -27,22 +45,41 @@ public class PortalManager {
 		Portal portal = new Portal(id);
 		portal.setRegion(selection);
 		portal.setExitLocation(sender.getLocation().clone());
-		getInstance().registerPortal(portal);		
+		getInstance().registerPortal(portal);
 	}
-	
+
 	private Set<Portal> portals = null;
-	
+
 	public PortalManager() {
 		portals = new HashSet<Portal>();
 	}
-	
+
 	public Portal getPortalFromLocation(Location loc) {
 		for (Portal portal : portals) {
-			if (portal.getPortalRegion().contains(new Vector(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()))) {
+			
+			// Check everything
+			if (portal.getPortalRegion().contains(new Vector(loc.getX(), 
+																loc.getY(),
+																loc.getZ()))) {
+				bPortals.getInstance().debugConsole("Standard location");
+				return portal;
+			}
+			
+			if (portal.getPortalRegion().contains(new Vector(NumberConversions.ceil(loc.getX()), 
+																NumberConversions.ceil(loc.getY()),
+																NumberConversions.ceil(loc.getZ())))) {
+				bPortals.getInstance().debugConsole("Ceil location");
+				return portal;
+			}
+			
+			if (portal.getPortalRegion().contains(new Vector(NumberConversions.floor(loc.getX()),
+																NumberConversions.floor(loc.getY()),
+																NumberConversions.floor(loc.getZ())))) {
+				bPortals.getInstance().debugConsole("Floor location");
 				return portal;
 			}
 		}
-		
+
 		return null;
 	}
 
@@ -52,10 +89,10 @@ public class PortalManager {
 				return portal;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public void registerPortal(Portal portal) {
 		portals.add(portal);
 	}
@@ -71,7 +108,7 @@ public class PortalManager {
 	public boolean isPortal(Location location) {
 		return getPortalFromLocation(location) != null;
 	}
-	
+
 	void destroy() {
 		portals.clear();
 		portals = null;
