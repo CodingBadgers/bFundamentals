16e16d4022fc94b33298a745066fafef09db9866
SamOatesGames <sam@samoatesgames.com>
2013-09-06 22:42:22 +0100

    Went through bCreative commenting the code and tidying up some places.
     - Store the player from the event. In c++ calling .getPlayer() will add a function call to the stack which is 4 bytes per call. We are talking nano seconds, but we might as well be optimized.
     - Made it so loading the material blacklist protects against non-existant materials
     - Made it so all world names are lowercase
     - Added an item player cache, so pickup deny messages are only shown if a player is attempting to pickup a different item
     - Changed some perm names and messages.

--- bCreative/src/uk/codingbadgers/bcreative/bCreative.java
+++ bCreative/src/uk/codingbadgers/bcreative/bCreative.java
@@ -19,6 +19,7 @@ package uk.codingbadgers.bcreative;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -26,6 +27,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -40,10 +43,26 @@ import uk.codingbadgers.bFundamentals.player.FundamentalPlayer;
 
 public class bCreative extends Module implements Listener {
 
-	private List<String> activeWorlds;
-	private List<Material> interactBlacklist;
+	/**
+	 * The list of worlds that bCreative is active in
+	 */
+	private List<String> activeWorlds = null;
+	
+	/**
+	 * The list of black listed interact materials
+	 */
+	private List<Material> interactBlacklist = null;
+	
+	/**
+	 * A hashmap of the last item a player tried to pickup
+	 */
+	private HashMap<Player, Item> playersLastPickupItem = null;
 
+	/* 
+	 * Called when the module is enabled
+	 */
 	@Override
+	@SuppressWarnings("deprecation")
 	public void onEnable() {
 		register(this);
 		
@@ -53,113 +72,195 @@ public class bCreative extends Module implements Listener {
 		config.options().copyDefaults(true);
 		saveConfig();
 		
-		activeWorlds = config.getStringList("worlds");
+		activeWorlds = new ArrayList<String>();
+		List<String> configWorlds = config.getStringList("worlds");
+		for (String worldName : configWorlds)
+			activeWorlds.add(worldName.toLowerCase());
+		
+		log(Level.INFO, "bCreative is active in the following worlds:");
+		for (String worldName : activeWorlds)
+			log(Level.INFO, " - " + worldName);
+		
 		interactBlacklist = new ArrayList<Material>();
-		for (String mat : config.getStringList("interact-blacklist")) {
+		for (String materialId : config.getStringList("interact-blacklist")) {
 			try {
-				int id = Integer.parseInt(mat);
-				interactBlacklist.add(Material.getMaterial(id));
-			} catch (Exception ex) {
-				interactBlacklist.add(Material.getMaterial(mat));
+				int id = Integer.parseInt(materialId);
+				Material interactMaterial = Material.getMaterial(id);
+				if (interactMaterial != null) {
+					interactBlacklist.add(interactMaterial);
+				}
+			} catch (NumberFormatException ex) {
+				Material interactMaterial = Material.getMaterial(materialId);
+				if (interactMaterial == null) {
+					interactMaterial = Material.matchMaterial(materialId);
+				}
+				if (interactMaterial != null) {
+					interactBlacklist.add(interactMaterial);
+				}
 			}
 		}
-		log(Level.INFO, activeWorlds.toString());
+		log(Level.INFO, "bCreative will block interaction with the following materials:");
+		for (Material material : interactBlacklist)
+			log(Level.INFO, " - " + material.name());
+		
+		playersLastPickupItem = new HashMap<Player, Item>();
 	}
 
+	/* 
+	 * Called when the module is disabled
+	 */
 	@Override
 	public void onDisable() {
 		activeWorlds.clear();
 		interactBlacklist.clear();
+		playersLastPickupItem.clear();
 	}
 	
+	/* 
+	 * Called when a player interacts with something
+	 */
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		if (hasPermission(event.getPlayer(), "bcreative.chest.open")) {
+		
+		final Player player = event.getPlayer();
+		
+		// If the player has the interact permission, let them do what they want
+		if (hasPermission(player, "bcreative.player.interact")) {
 			return;
 		}
 		
-		if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+		// We only care about creative players
+		if (player.getGameMode() != GameMode.CREATIVE) {
 			return;
 		}
 
+		// We only care about right click events
 		if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
 			return;
 		}
 		
-		if (!activeWorlds.contains(event.getPlayer().getWorld().getName())) { 
+		// Is the player in a world where bCreative is active?
+		if (!activeWorlds.contains(player.getWorld().getName().toLowerCase())) { 
 			return;
 		}
 		
-		if (!interactBlacklist.contains(event.getClickedBlock().getType())) {
+		// Is the interacted material in our blacklist?
+		final Material material = event.getClickedBlock().getType();
+		if (!interactBlacklist.contains(material)) {
 			return;
 		}
 		
+		// Cancel the event as the material is on the blacklist
 		event.setCancelled(true);
-		sendMessage(getName(), event.getPlayer(), "You cannot open chest's whilst in creative");
+		sendMessage(getName(), player, "You cannot open '" + material.name() + "' whilst in creative mode.");
 	}
 
+	/* 
+	 * Called when a player drops an item
+	 */
 	@EventHandler
 	public void onPlayerItemDrop(PlayerDropItemEvent event) {
-		if (hasPermission(event.getPlayer(), "bcreative.item.drop")) {
+		
+		final Player player = event.getPlayer();
+		
+		// If the player has this permission let them drop items
+		if (hasPermission(player, "bcreative.player.item.drop")) {
 			return;
 		}
 		
-		if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+		// We only care about creative mode
+		if (player.getGameMode() != GameMode.CREATIVE) {
 			return;
 		}
 		
-		if (!activeWorlds.contains(event.getPlayer().getWorld().getName())) { 
+		// See if they are in a world where bCreative is active
+		if (!activeWorlds.contains(player.getWorld().getName().toLowerCase())) { 
 			return;
 		}
 		
+		// Cancel the drop event
 		event.setCancelled(true);
-		sendMessage(getName(), event.getPlayer(), "You cannot drop items whilst in creative");
+		sendMessage(getName(), player, "You cannot drop items whilst in creative mode.");
 	}
 	
+	/* 
+	 * Called when a player picks up an item
+	 */
 	@EventHandler
 	public void onPlayerItemPickup(PlayerPickupItemEvent event) {
-		if (hasPermission(event.getPlayer(), "bcreative.item.pickup")) {
+		
+		final Player player = event.getPlayer();
+		
+		// If the player has this permission, let them pick things up
+		if (hasPermission(player, "bcreative.player.item.pickup")) {
 			return;
 		}
 		
-		if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+		// We only care about creative mode
+		if (player.getGameMode() != GameMode.CREATIVE) {
 			return;
 		}
 		
-		if (!activeWorlds.contains(event.getPlayer().getWorld().getName())) { 
+		// Is bCreative active in this world?
+		if (!activeWorlds.contains(player.getWorld().getName().toLowerCase())) { 
 			return;
 		}
 		
+		// Cancel the pickup event
 		event.setCancelled(true);
-		// TODO work out better way to send message to player, this is spammy
-		//sendMessage(getName(), event.getPlayer(), "You cannot pickup items whilst in creative");
+		
+		// Get the item and the last item the player tried to pickup
+		final Item item = event.getItem();
+		final Item lastKnown = playersLastPickupItem.get(player);
+				
+		// If the player is trying to pickup a new item tell them they can't
+		if (item != lastKnown) {
+			
+			// If we have a last known, remove it
+			if (lastKnown != null) {
+				playersLastPickupItem.remove(player);
+			}
+			
+			// Update the last known item for this player
+			playersLastPickupItem.put(player, item);
+			
+			sendMessage(getName(), player, "You cannot pickup items whilst in creative mode.");
+		}
 	}
 
+	/* 
+	 * Called when a player teleports
+	 */
 	@EventHandler
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
-		if (hasPermission(event.getPlayer(), "bcreative.teleport")) {
+		
+		final Player player = event.getPlayer();
+		
+		// If the player has this permission let them teleport how they want
+		if (hasPermission(player, "bcreative.player.teleport")) {
 			return;
 		}
 		
+		// If they arn't changing world, let them teleport as normal
 		if (event.getTo().getWorld() == event.getFrom().getWorld()) {
 			return;
 		}
 		
-		FundamentalPlayer player = bFundamentals.Players.getPlayer(event.getPlayer());
+		FundamentalPlayer fundamentalPlayer = bFundamentals.Players.getPlayer(player);
 
 		// Teleporting to creative world
 		if (activeWorlds.contains(event.getTo().getWorld().getName())) {
 			// Order is everything
-			player.backupInventory(activeWorlds.contains(event.getFrom().getWorld()) ? event.getFrom().getWorld() : Bukkit.getWorlds().get(0), true);
-			player.restoreInventory(event.getTo().getWorld());
+			fundamentalPlayer.backupInventory(activeWorlds.contains(event.getFrom().getWorld()) ? event.getFrom().getWorld() : Bukkit.getWorlds().get(0), true);
+			fundamentalPlayer.restoreInventory(event.getTo().getWorld());
 			sendMessage(getName(), event.getPlayer(), "You're inventory has been backed up whilst you are in the creative world");
 		}
 
 		// Teleporting from creative world
 		if (activeWorlds.contains(event.getFrom().getWorld().getName())) {	
 			// Order is everything		
-			player.backupInventory(event.getFrom().getWorld(), true);			
-			player.restoreInventory(activeWorlds.contains(event.getTo().getWorld()) ? event.getTo().getWorld() : Bukkit.getWorlds().get(0));
+			fundamentalPlayer.backupInventory(event.getFrom().getWorld(), true);			
+			fundamentalPlayer.restoreInventory(activeWorlds.contains(event.getTo().getWorld()) ? event.getTo().getWorld() : Bukkit.getWorlds().get(0));
 			sendMessage(getName(), event.getPlayer(), "You're inventory has been restored now you have left the creative world");
 		}
 	}
