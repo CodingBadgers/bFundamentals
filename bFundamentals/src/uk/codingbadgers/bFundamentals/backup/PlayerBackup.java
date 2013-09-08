20c66e25a81957cd59a403b08f9fcaeaea28ec7e
SamOatesGames <sam@samoatesgames.com>
2013-09-08 11:07:50 +0100

    Changed player backups to take a folder to save the backup in.
    This means it is up to a module to decide where the player backups should be rather than the plugin.
    Also fixed a few typos and commented the crap out of everything.

--- bFundamentals/src/uk/codingbadgers/bFundamentals/backup/PlayerBackup.java
+++ bFundamentals/src/uk/codingbadgers/bFundamentals/backup/PlayerBackup.java
@@ -28,9 +28,11 @@ import java.util.Map;
 
 import org.apache.commons.lang.Validate;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -39,34 +41,76 @@ import org.json.simple.parser.ParseException;
 @SuppressWarnings("unchecked")
 public class PlayerBackup {
 
-	private String name;
-	private File backup;
+	private String name = null;
+	private File backup = null;
 	
 	private ItemStack[] invContents = null;
 	private ItemStack[] armourContents = null;
 	private int xp = 0;
-	private int gamemode = 0;
+	private String gamemode = null;
 	
-	public PlayerBackup(Player player) throws IOException {
+	/**
+	 * Get the players armour inventory stack
+	 *
+	 * @param player the player to query
+	 * @return A itemstack array of the players armour
+	 */
+	private ItemStack[] getPlayerArmour(Player player) {
 		
-		if (!BackupFactory.BACKUP_DIR.exists()) {
-			BackupFactory.BACKUP_DIR.mkdirs();
-		}
+		final PlayerInventory invent = player.getInventory();
+		ItemStack[] armour = new ItemStack[4];
 		
+		armour[0] = invent.getHelmet();
+		armour[1] = invent.getChestplate();
+		armour[2] = invent.getLeggings();
+		armour[3] = invent.getBoots();
+		
+		return armour;
+	}
+	
+	/**
+	 * Set the players armour inventory stack
+	 *
+	 * @param player the player to query
+	 */
+	private void setPlayerArmour(Player player, ItemStack[] armour) {
+		
+		final PlayerInventory invent = player.getInventory();
+		
+		invent.setHelmet(armour[0]);
+		invent.setChestplate(armour[1]);
+		invent.setLeggings(armour[2]);
+		invent.setBoots(armour[3]);
+	}
+	
+	/**
+	 * Create a player backup from a given player and write it to a specified file
+	 *
+	 * @param backupFile The file where the player backup should be written
+	 * @param player The player to backup
+	 */
+	public PlayerBackup(File backupFile, Player player) throws IOException {
+				
 		this.name = player.getName();
 		this.invContents = player.getInventory().getContents();
-		this.armourContents = player.getInventory().getArmorContents();
+		this.armourContents = getPlayerArmour(player);
 		this.xp = player.getTotalExperience();
-		this.gamemode = player.getGameMode().getValue();
-		this.backup = new File(BackupFactory.BACKUP_DIR, player.getWorld().getName().replace(File.separatorChar, '_') + File.separatorChar + name.replace(File.separatorChar, '_') + ".json");
+		this.gamemode = player.getGameMode().name();
+		this.backup = backupFile;
 		
-		if (!this.backup.getParentFile().exists()) {
-			this.backup.getParentFile().mkdir();
+		File backupFolder = backupFile.getParentFile();
+		if (!backupFolder.exists()) {
+			backupFolder.mkdirs();
 		}
 		
 		writeToFile();
 	}
 	
+	/**
+	 * Parse a given file to create a player backup object
+	 *
+	 * @param backupFile The file where the player backup should be read from
+	 */
 	public PlayerBackup(File file) throws IOException {
 		Validate.notNull(file, "Backup file cannot be null");
 		Validate.isTrue(file.exists(), "Backup file must exist");
@@ -76,21 +120,34 @@ public class PlayerBackup {
 		readFromFile();
 	}
 	
+	/**
+	 * Get the player name this player backup represents
+	 *
+	 * @return The player name this backup represents
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Restore a players backup
+	 *
+	 * @param player The player who we are restoring too
+	 */
 	public void restore(Player player) {
 		if (!player.getName().equalsIgnoreCase(name)) {
 			return;
 		}
-		
+
 		player.getInventory().setContents(invContents);
-		player.getInventory().setArmorContents(armourContents);
+		setPlayerArmour(player, armourContents);
 		player.setTotalExperience(xp);
-		player.setGameMode(GameMode.getByValue(gamemode));
+		player.setGameMode(GameMode.valueOf(gamemode));
 	}
 	
+	/**
+	 * Write a player backup to file
+	 */
 	public void writeToFile() throws IOException {
 		
 		if (!backup.exists()) {
@@ -101,9 +158,9 @@ public class PlayerBackup {
 		
 		obj.put("name", name);
 		obj.put("level", String.valueOf(xp));
-		obj.put("inventory", convertInvToArray(invContents));
-		obj.put("armour", convertInvToArray(armourContents));
-		obj.put("gamemode", String.valueOf(gamemode));
+		obj.put("inventory", ItemStackArrayToJSONArray(invContents));
+		obj.put("armour", ItemStackArrayToJSONArray(armourContents));
+		obj.put("gamemode", gamemode);
 		
 		FileWriter stream = null;
 		BufferedWriter writer = null;
@@ -120,9 +177,11 @@ public class PlayerBackup {
 		}
 	}
 	
+	/**
+	 * Read a player backup from file
+	 */
 	public void readFromFile() throws IOException {
-		
-		// should never come to this
+
 		if (backup == null || !backup.exists()) {
 			return;
 		}
@@ -135,10 +194,10 @@ public class PlayerBackup {
 			JSONObject object = (JSONObject) parser.parse(reader);
 			
 			name = (String) object.get("name");
-			invContents = convertArrayToInv((JSONArray) object.get("inventory"));
-			armourContents = convertArrayToInv((JSONArray) object.get("armour"));
+			invContents = JSONArrayToItemStackArray((JSONArray) object.get("inventory"));
+			armourContents = JSONArrayToItemStackArray((JSONArray) object.get("armour"));
 			xp = Integer.valueOf((String) object.get("level"));
-			gamemode = Integer.valueOf((String) object.get("gamemode"));
+			gamemode = (String) object.get("gamemode");
 		} catch (ParseException e) {
 			e.printStackTrace();
 		} finally {
@@ -148,29 +207,33 @@ public class PlayerBackup {
 		}
 	}
 	
-	private JSONArray convertInvToArray(ItemStack[] items) {
+	/**
+	 * Convert an itemstack array into a json array
+	 *
+	 * @param items The itemstack array to convert
+	 * @return A Json array of the given itemstack array
+	 */
+	private JSONArray ItemStackArrayToJSONArray(ItemStack[] items) {
 
 		JSONArray inv = new JSONArray();
 		
-		for (ItemStack stack : invContents) {
+		for (ItemStack stack : items) {
 			JSONObject item = new JSONObject();
 			
 			if (stack == null) {
 				continue;
 			}
 			
-			item.put("id", String.valueOf(stack.getTypeId()));
+			item.put("id", stack.getType().name());
 			item.put("amount", String.valueOf(stack.getAmount()));
 			
 			JSONArray enchants = new JSONArray();
-			
 			for (Map.Entry<Enchantment, Integer> entry : stack.getEnchantments().entrySet()) {
 				JSONObject enchantment = new JSONObject();
-				enchantment.put("id", String.valueOf(entry.getKey().getId()));
+				enchantment.put("id", entry.getKey().getName());
 				enchantment.put("level", String.valueOf(entry.getValue()));
 				enchants.add(enchantment);
 			}
-			
 			item.put("enchant", enchants);
 			
 			if (stack.getItemMeta().hasDisplayName()) {
@@ -183,7 +246,13 @@ public class PlayerBackup {
 		return inv;
 	}
 	
-	private ItemStack[] convertArrayToInv(JSONArray array) {
+	/**
+	 * Convert a json array into an itemstack array
+	 *
+	 * @param array The json array to convert
+	 * @return An itemstack array of the given json array
+	 */
+	private ItemStack[] JSONArrayToItemStackArray(JSONArray array) {
 		
 		List<ItemStack> items = new ArrayList<ItemStack>(array.size());
 		
@@ -195,7 +264,7 @@ public class PlayerBackup {
 			
 			JSONObject obj = (JSONObject) stack;
 			
-			ItemStack item = new ItemStack(Integer.valueOf((String) obj.get("id")));
+			ItemStack item = new ItemStack(Material.valueOf((String) obj.get("id")));
 			item.setAmount(Integer.valueOf((String) obj.get("amount")));
 			
 			for (Object enchObj : (JSONArray) obj.get("enchant")) {
@@ -206,7 +275,7 @@ public class PlayerBackup {
 				
 				JSONObject ench = (JSONObject) enchObj;
 				
-				Enchantment enchantment = Enchantment.getById(Integer.valueOf((String) ench.get("id")));
+				Enchantment enchantment = Enchantment.getByName((String) ench.get("id"));
 				item.addEnchantment(enchantment, Integer.valueOf((String) ench.get("level")));
 			}
 			
@@ -220,6 +289,11 @@ public class PlayerBackup {
 		return items.toArray(new ItemStack[0]);
 	}
 	
+	/**
+	 * Delete the backup file
+	 * 
+	 * @return True if the file was deleted, false otherwise
+	 */
 	public boolean deleteFile() {
 		return backup.delete();
 	}
