5933c366eb13d5cb584ccd9d9eabd7cf91b4e5d1
samoatesgames <sam@samoatesgames.com>
2014-04-05 10:09:31 +0100

    Updated bRewarded.

---
+++ bRewarded/src/uk/codingbadgers/brewarded/VoteTableData.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.codingbadgers.brewarded;
+
+import uk.thecodingbadgers.bDatabaseManager.DatabaseTable.DatabaseTableData;
+
+/**
+ *
+ * @author Sam
+ */
+public class VoteTableData extends DatabaseTableData {
+	
+	/**
+	 * 
+	 */
+	public String server;
+	
+	/**
+	 * 
+	 */
+	public String username;
+	
+	/**
+	 * 
+	 */
+	public String service;
+	
+	/**
+	 * 
+	 */
+	public long timestamp;
+	
+}
